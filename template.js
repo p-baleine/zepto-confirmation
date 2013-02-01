7201a4dd0eabb37fae4df35a93b1f963653f1246
Tajima Junpei <p.baleine@gmail.com>
2013-02-01 19:41:55 +0900

    add style

--- template.js
+++ template.js
@@ -1,1 +1,1 @@
-module.exports = '<div id="confirmation" class="hide">\n  <div class="confirmation-title"><%= title %></div>\n  <div class="confirmation-message"><%= message %></div>\n  <button class="ok button"></button>\n  <button class="cancel button"></button>\n</div>\n';
+module.exports = '<div id="confirmation" class="hide">\n  <div class="confirmation-title"><%= title %></div>\n  <div class="confirmation-message"><%= message %></div>\n  <div class="confirmation-action">\n    <button class="ok button"></button>\n    <button class="cancel button"></button>\n  </div>\n</div>\n';
